431dcf0c0d35d9710360be4ada94bd5bf044ac5c
arjunsharma <bvm.arjun108@gmail.com>
2025-03-24 15:24:58 -0400

    "Refactor Changes"

--- src/main/java/com/poc/flyway/Multitenant_Flyway_POC/multitenant/testService/ClientTestDbRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poc.flyway.Multitenant_Flyway_POC.multitenant.testService;
-
-import com.adp.benefits.carrier.entity.client.ClientTestDb;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientTestDbRepository extends JpaRepository<ClientTestDb, String> {
-}
